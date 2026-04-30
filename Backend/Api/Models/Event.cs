09a6c418392779fbb030f8dde101dc64c2349ab5
ssanni <ssanni@taltech.ee>
2026-04-30 18:33:56 +0300

    Add Event and Registration models

---
+++ Backend/Api/Models/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace Api.Models;
+
+public class Event
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DateTime { get; set; }
+    public int MaxParticipants { get; set; }
+}
---
+++ Backend/Api/Models/Registration.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models;
+
+public class Registration
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PersonalCode { get; set; }
+    public int EventId { get; set; }
+    public Event Event { get; set; }
+}
